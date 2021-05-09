bc8f831f5f64f8a7cd7ce0a1c435a04838ab5abc
yodyindra <indrayody04@gmail.com>
2021-05-09 15:02:18 +0800

    Ditambahkan fungsi OneWire oneWire(4);

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 const char* WIFI_SSID = "Zoker";
 const char* WIFI_PASS = "gus ardhi";
 const char* HOSTNAME = "FTI";
+
+OneWire oneWire(4);
+DallasTemperature sensors(&oneWire);
+
+float getAmbientTemperature(){
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures();
+  Serial.println("DONE");
+
+  float tempC = sensors.getTempCByIndex(0);
+
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+    return tempC;
+  } 
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+    return -127;
+  }
+}
 
 void setup()
 {
@@ -21,10 +46,13 @@ void setup()
   Serial.println("");
   Serial.println("WiFi connected successfully.");
 
+  sensors.begin();
+
 }
 
 void loop()
 {
-  Serial.println("Hello Gus Ardhi");
-  delay(1000);
+  Serial.println("Hello Yody");
+  float suhu = getAmbientTemperature();
+  delay(3000);
 }
