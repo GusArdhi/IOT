72f48da1d305efe2e6790126f6d860de329a625b
ElaChandra <angelajchandra2001@gmail.com>
2021-05-17 00:14:59 +0800

    Integrasi IoT Broker dan Setup Pub/Sub Topic

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <MQTT.h>
 
 const char* WIFI_SSID = "Zoker";
 const char* WIFI_PASS = "gus ardhi";
 const char* HOSTNAME = "FTI";
+const char* IOTBROKER = "broker.hivemq.com";
+#define PIN_RELAY 32
+
+OneWire oneWire(4);
+DallasTemperature sensors(&oneWire);
+WiFiClient net;
+MQTTClient iot;
 
 void setup()
 {
+  pinMode(PIN_RELAY, OUTPUT);
   Serial.begin(115200);
   
   WiFi.mode(WIFI_STA);
@@ -21,10 +32,78 @@ void setup()
   Serial.println("");
   Serial.println("WiFi connected successfully.");
 
+  sensors.begin();
+  
+  iot.begin(IOTBROKER, net);
+  iot.onMessage(messageReceived);
+
+   Serial.print("Connecting to Iot Broker");
+  while(!iot.connect("ESP32", "public", "public"))
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.println("Iot broker connected successfully.");
+
+  iot.subscribe("undiknas/ti/kelompok4/relay");
+  iot.subscribe("undiknas/ti/+/chatroom");
+}
+unsigned long intervalCounterRelay = 0;
+unsigned long intervalCounterSensor = 0; 
+void loop() {
+  
+  unsigned long now = millis();
+  if( ( now - intervalCounterRelay) > 1000)
+  {
+    intervalCounterRelay = now;
+
+    bool relayState = getRelay();
+    iot.publish("undiknas/ti/kelompok4/relay/state", String(relayState));
+  }
+  
+  if( ( now - intervalCounterSensor) > 5000)
+  {
+    intervalCounterSensor = now;
+
+    Serial.println("Hello Gus Ardhi");
+    iot.publish("undiknas/ti/kelompok4/sensor/serial", String(Serial));
+  Serial.println("Hello Gus Ardhi");
+iot.loop();
 }
 
-void loop()
+
+
+void setRelay(bool state)
 {
-  Serial.println("Hello Gus Ardhi");
-  delay(1000);
+  digitalWrite(PIN_RELAY, state);
+  Serial.print("Relay state changed to: ");
+  Serial.print(state);
+  Serial.println();
+}
+
+bool getRelay()
+{
+  bool state = digitalRead(PIN_RELAY);
+  Serial.print("Relay state is: ");
+  Serial.print(state);
+  Serial.println();
+  return state;
+}
+
+void messageReceived(String &topic, String &payload)
+{
+  Serial.println("Incomming: " + topic + " - " + payload);
+
+  if(topic == "undiknas/ti/kelompok4/relay")
+  {
+    if(payload == "on")
+    {
+      setRelay(1);
+    }
+    else
+    {
+    setRelay(0);
+    }
+  }
 }
